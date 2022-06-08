4f05202508fe64b18184725217cf0dec2843bef9
avagadro <fragarie@yandex.com>
2022-06-08 20:23:38 +0300

    add packages share; optimize mount script

--- dagmount.sh
+++ dagmount.sh
@@ -1,17 +1,12 @@
-# mount DAGs
-if mountpoint -q ~/projects/airflow.env/dags/
-then
-    sudo umount ~/projects/airflow.env/dags/
-    echo "airflow DAGs folder was unmounted"
-else
-    sudo mount --bind ~/projects/$1/dags/ ~/projects/airflow.env/dags/ && echo "$1 project DAG mounted to airflow"
-fi
+mountFolder() {
+    if mountpoint -q ./$2
+    then
+        sudo umount ./$2
+        echo "$1 was unmounted"
+    else
+        sudo mount --bind $1/$2 ./$2 && echo "$1 folder mounted to airflow"
+    fi    
+}
 
-# mount data
-if mountpoint -q ~/projects/airflow.env/data/
-then
-    sudo umount ~/projects/airflow.env/data/
-    echo "airflow data folder was unmounted"
-else
-    sudo mount --bind ~/projects/$1/data/ ~/projects/airflow.env/data/ && echo "$1 project data mounted to airflow"
-fi
+mountFolder $1 dags
+mountFolder $1 data
